6a178a1c236de7b35c2f45ac4b3e4f06562748fb
Ibrahem Taha <Ibrahem.Taha@acronis.com>
2022-06-16 18:31:04 +0300

    newer

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -6,15 +6,17 @@ from pageObjects.LoginPage import LoginPage
 import time
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class Test_001_Login:
     baseURL = ReadConfig.getApplicationURL()
     username = ReadConfig.getUseremail()
     password = ReadConfig.getPassword()
 
-    def test_homePageTitle(self,setup):
+    def test_homePageTitle(self, setup):
         self.driver = setup
+        self.driver.implicitly_wait(25)
         self.driver.get(self.baseURL)
-        act_title=self.driver.title
+        act_title = self.driver.title
         self.driver.close()
         if act_title == "Login":
             assert True
@@ -23,7 +25,7 @@ class Test_001_Login:
 
     def test_login(self, setup):
         self.driver = setup
-        self.driver.implicitly_wait(10) #
+        self.driver.implicitly_wait(25)
         self.driver.get(self.baseURL)
 
         self.lp = LoginPage(self.driver)
@@ -38,9 +40,3 @@ class Test_001_Login:
             assert True
         else:
             assert False
-
-
-
-
-
-
